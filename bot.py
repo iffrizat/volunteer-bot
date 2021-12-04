7e1e4a525b60a85dbcc0026a127088cb01fbf1fc
iffrizat <fork1791@gmail.com>
2021-12-04 15:45:48 +0300

    added answer support

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 from telegram import Update
 from telegram.ext import CallbackContext, Updater, MessageHandler, Filters
+from tfidf import choose_answer
 
 updater = Updater(token="epic_token")
 dispatcher = updater.dispatcher
 
 # Простое повторение сообщения
 def echo(update: Update, context: CallbackContext):
-    update.message.reply_text(update.message.text)
+    answer = choose_answer(update.message.text)
+    update.message.reply_text(answer)
 
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
